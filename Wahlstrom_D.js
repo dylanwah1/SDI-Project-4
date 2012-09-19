2398d896cf7376a9e971e29c3f56b6e5547d8e14
dylan wahlstrom <dylanwah1@fullsail.edu>
2012-09-18 21:35:34 -0400

    first function

---
+++ Wahlstrom_D.js
@@ -0,0 +1,31 @@
+// Dylan R. Wahlstrom	
+// SDI 1209
+//Project 4
+//Library of functions
+
+var myLibrary = function(){
+
+var checkString = function(val){
+
+	var telNumber = val.length
+	for(var i=0; i <= telnumber; i++){
+		if(val.substring(i, i+1) === "-"){
+			return true;
+		} else {
+			return false;
+		};
+
+
+
+	return {
+		"checkString":checkString
+	
+	
+	};
+
+};
+
+var newLib = new myLibrary();{
+
+console.log(newLib.checkString "404-555-3366");
+
