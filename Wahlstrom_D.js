71f1ec43010e81e870320b3633db77a14e787970
dylan wahlstrom <dylanwah1@fullsail.edu>
2012-09-19 11:02:50 -0400

    Not sure if this works for the day between day?

--- Wahlstrom_D.js
+++ Wahlstrom_D.js
@@ -17,6 +17,17 @@ var MyLibrary = function() {
             }
         }
     };
+
+today=new Date()
+var myBirthday=new Date(today.getFullYear(), 11, 25) 
+if (today.getMonth()==11 && today.getDate()>25) 
+myBirthday.setFullYear(myBirthday.getFullYear()+1) 
+var one_day=1000*60*60*24
+
+
+console.log(Math.ceil((myBirthday.getTime()-today.getTime())/(one_day))+
+" days left until birthday!")
+    
     
     return {
         "checkString": checkString
