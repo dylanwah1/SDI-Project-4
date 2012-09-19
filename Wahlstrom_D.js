1e20a8d42ec7f8fc4d58762c5a8fe89d319aed19
dylan wahlstrom <dylanwah1@fullsail.edu>
2012-09-18 22:23:53 -0400

    First function complete!

--- Wahlstrom_D.js
+++ Wahlstrom_D.js
@@ -3,30 +3,26 @@
 //Project 4
 //Library of functions
 
-var myLibrary = function(){
 
-var checkString = function(val){
+var MyLibrary = function() {
 
-	var telNumber = val.length;
-	for(var i=0; i <= telnumber; i++){
-		if(val.substring(i, i+1) === "-"){
-			return true;
-		} else {
-			return false;
-		};
-		
+    var checkString = function(val) {
+        var telNumber = val.length;
 
-
-
-	return {
-		"checkString":checkString
-	
-	
-	};
-
+        for (var i = 0; i < telNumber; i++) {
+            if (val.substring(i, i + 0) === "-") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+    
+    return {
+        "checkString": checkString
+    };
 };
 
-var newLib = new myLibrary();
+var newLib = new MyLibrary();
 
 console.log(newLib.checkString("404-555-3366"));
-};
